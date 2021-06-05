a10e0d07cd0d02a51c9ac66fa0d11ef47fa5ac91
esTechVMG <vicente.maroto@escuelaestech.es>
2021-06-06 00:25:38 +0200

    Add list commerces entrypoint

--- app/src/main/java/com/iqueueteam/i_queue/entry/IQueue/Repository/IQueueService.kt
+++ app/src/main/java/com/iqueueteam/i_queue/entry/IQueue/Repository/IQueueService.kt
@@ -1,20 +1,21 @@
 package com.iqueueteam.i_queue.entry.IQueue.Repository
 
+import com.iqueueteam.i_queue.entry.IQueue.Models.IQCommerce
 import com.iqueueteam.i_queue.entry.IQueue.Models.IQResponse
 import com.iqueueteam.i_queue.entry.IQueue.Models.IQUser
 import com.iqueueteam.i_queue.entry.IQueue.Models.IQValidationError
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 
 interface IQueueService {
     @Headers("Accept: application/json","Content-Type: application/json")
-    //@FormUrlEncoded
+    //@FormUrlEncodedAny
     //@Headers("Content-Type: application/x-www-form-urlencoded")
     @POST("login")
     suspend  fun doLogin(@Body user: LoginUser): Response<IQResponse<IQUser?,IQValidationError?>>
+
+    @GET("commerces")
+    suspend fun getCommerce(): Response<IQResponse<Array<IQCommerce>,Any?>>
 
 }
