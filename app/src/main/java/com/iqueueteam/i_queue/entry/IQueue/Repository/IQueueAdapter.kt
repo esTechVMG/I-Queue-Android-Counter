ed2e9332df49e740437f4b306defbe93b0368954
Dev C-512L <vincentc512l@gmail.com>
2021-06-06 15:20:15 +0200

    Separate getting response in its own generic function

--- app/src/main/java/com/iqueueteam/i_queue/entry/IQueue/Repository/IQueueAdapter.kt
+++ app/src/main/java/com/iqueueteam/i_queue/entry/IQueue/Repository/IQueueAdapter.kt
@@ -1,8 +1,14 @@
 package com.iqueueteam.i_queue.entry.IQueue.Repository
 
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,4 +17,17 @@ object IQueueAdapter {
     val apiClient:IQueueService = Retrofit.Builder().baseUrl("http://10.0.2.2/api/").client(e)
         .addConverterFactory(GsonConverterFactory.create()).build()
         .create(IQueueService::class.java)
+    public fun <T> getResponse(response: Response<T>): T? {
+        if (response.isSuccessful){
+            return response.body()
+        }else{
+            CoroutineScope(Dispatchers.IO).launch {
+                runCatching {
+                    val listType = object : TypeToken<T>() {}.type
+                    return@launch Gson().fromJson(response.errorBody()?.string(),listType)
+                }
+            }
+        }
+        throw Exception("Unexpected Error. Could not parse body")
+    }
 }
--- app/src/main/java/com/iqueueteam/i_queue/entry/MainActivity.kt
+++ app/src/main/java/com/iqueueteam/i_queue/entry/MainActivity.kt
@@ -10,58 +10,49 @@ import com.google.gson.reflect.TypeToken
 import com.iqueueteam.i_queue.entry.IQueue.Models.IQResponse
 import com.iqueueteam.i_queue.entry.IQueue.Models.IQUser
 import com.iqueueteam.i_queue.entry.IQueue.Models.IQValidationError
+import com.iqueueteam.i_queue.entry.IQueue.Repository.IQueueAdapter
 import com.iqueueteam.i_queue.entry.IQueue.Repository.IQueueAdapter.apiClient
 import com.iqueueteam.i_queue.entry.IQueue.Repository.LoginUser
 import kotlinx.coroutines.*
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     private lateinit var sendButton: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("I-Queue", "Build Type: ${BuildConfig.BUILD_TYPE}")
-
         sendButton = findViewById(R.id.send_button)
         sendButton.setOnClickListener {
-            launch(Dispatchers.IO) {
-                // Try catch block to handle exceptions when calling the API.
-                try {
-                    var myBody:IQResponse<IQUser?,IQValidationError?>? = IQResponse(0,"",null,null)
-                    val response = apiClient.doLogin(LoginUser("test@gmail.com","12345"))
 
-                    if (response.isSuccessful){
-                        myBody = response.body()
-                    }else{
-                        CoroutineScope(Dispatchers.IO).launch {
-                            runCatching {
-                                val listType = object : TypeToken<IQResponse<IQUser,IQValidationError?>?>() {}.type
-                                myBody = Gson().fromJson(response.errorBody()?.string(),listType)
-                            }
+        }
+    }
+    fun re(){
+        launch(Dispatchers.IO) {
+            // Try catch block to handle exceptions when calling the API.
+            try {
+                val response = apiClient.doLogin(LoginUser("test@gmail.com","12345"))
+                val myBody:IQResponse<IQUser?,IQValidationError?>? = IQueueAdapter.getResponse(response)
+                myBody?.let {
+                    when (true){
+                        it.code in 200..299 -> {
+                            Log.d("Request","Request Code:${it.code}")
                         }
-                    }
-                    Log.d("Hello","World")
-                    myBody?.let {
-                        when (true){
-                            it.code in 200..299 -> {
-                                Log.d("Request","Request Code:${it.code}")
-                            }
-                            else -> {
-                                Log.d("Request","Unexpected Error")
-                            }
-                        }
-                    }
-                    } catch (e: Exception){
-                        launch(Dispatchers.Main) {
-                            // Show API error.
-                            Toast.makeText(this@MainActivity,
-                                "Error Occurred: ${e.message}",
-                                Toast.LENGTH_LONG).show()
-                            Log.d("Error Occurred:", "${e.message}")
+                        else -> {
+                            Log.d("Request","Unexpected Error")
                         }
                     }
                 }
+            } catch (e: Exception){
+                launch(Dispatchers.Main) {
+                    // Show API error.
+                    Toast.makeText(this@MainActivity,
+                        "Error Occurred: ${e.message}",
+                        Toast.LENGTH_LONG).show()
+                    Log.d("Error Occurred:", "${e.message}")
+                }
+            }
         }
     }
 }
