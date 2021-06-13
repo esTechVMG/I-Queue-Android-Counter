b0ce0e0d1f713ed44bfa5a82ac6c514f6f6d91a6
esTechVMG <vicente.maroto@escuelaestech.es>
2021-06-14 01:37:20 +0200

    Gson SharedPreferences Object wrapper

---
+++ app/src/main/java/com/iqueueteam/i_queue/entry/config_storage/SharedPreferencesGson.kt
@@ -0,0 +1,31 @@
+package com.iqueueteam.i_queue.entry.config_storage
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import com.google.gson.Gson
+import java.lang.Exception
+import kotlin.reflect.KClass
+
+class SharedPreferencesGson(
+    val context: Context,
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences("objects",Context.MODE_PRIVATE)
+) {
+    private val gson:Gson = Gson();
+    fun <T : Any> getObjectFromSharedPref(objectType:KClass<T>,key:String){
+        val stringToParse: String? = sharedPreferences.getString(key,"")
+        val objectToReturn = gson.fromJson(stringToParse,objectType.java)
+        objectToReturn.let {
+            return@let it
+        } ?: kotlin.run {
+            throw Exception("Could Not Parse Object")
+        }
+    }
+    fun <T : Any> setObjectToSharedPref(objectToParse:T, key:String){
+        val stringToStore:String = gson.toJson(objectToParse);
+        sharedPreferences.edit {
+            this.putString(key,stringToStore)
+            apply()
+        }
+    }
+}
