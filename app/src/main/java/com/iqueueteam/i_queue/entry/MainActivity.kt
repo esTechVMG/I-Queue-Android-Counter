e5020b97abfd7252e7d586947e7b8cb97210f14b
esTechVMG <vicente.maroto@escuelaestech.es>
2021-06-14 14:23:54 +0200

    refactor null checking

--- app/src/main/java/com/iqueueteam/i_queue/entry/MainActivity.kt
+++ app/src/main/java/com/iqueueteam/i_queue/entry/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.iqueueteam.i_queue.entry
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.iqueueteam.i_queue.entry.config_storage.SharedPreferencesGson
 import com.iqueueteam.i_queue.entry.databinding.ActivityMainBinding
 import com.iqueueteam.i_queue.entry.iqueue.models.IQResponse
 import com.iqueueteam.i_queue.entry.iqueue.models.IQUser
@@ -15,41 +14,50 @@ import com.iqueueteam.i_queue.entry.iqueue.repository.IQueueAdapter
 import com.iqueueteam.i_queue.entry.iqueue.repository.IQueueAdapter.apiClient
 import com.iqueueteam.i_queue.entry.iqueue.repository.LoginUser
 import kotlinx.coroutines.*
-import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     private lateinit var binding: ActivityMainBinding
 
-    val sharedPref = getSharedPreferences(
-        getString(R.string.token_storage), Context.MODE_PRIVATE)
+    private lateinit var sharedPreferencesGson:SharedPreferencesGson
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        sharedPreferencesGson = SharedPreferencesGson(this)
         Log.d("I-Queue", "Build Type: ${BuildConfig.BUILD_TYPE}")
-
+        try {
+            val iqUser:IQUser = sharedPreferencesGson.getObjectFromSharedPref(IQUser::class,getString(R.string.user_info_storage))
+            Log.d(getString(R.string.app_name),"Login Credentials retreived. Trying to get commerce and queue info from server")
+            //TODO request data and pass to next screen in that case
+        }catch (e:Exception){
+            Log.d(getString(R.string.app_name),"Could not retrieve user data")
+        }
 
         binding.sendButton.setOnClickListener {
             login()
         }
 
     }
-    fun login(){
+    private fun login(){
         launch(Dispatchers.IO) {
             // Try catch block to handle exceptions when calling the API.
             try {
-                val response = apiClient.doLogin(LoginUser("test@gmail.com","12345"))
+                //TODO Make validations
+                val response = apiClient.doLogin(LoginUser("" ,"12345"))
                 val body:IQResponse<IQUser?,IQValidationError?> = IQueueAdapter.getResponse(response) ?: throw Exception(getString(R.string.error_connecting_server))
                 when (true){
                     body.code in 200..299 -> {
                         Log.d("Request","Request Code:${body.code}")
-                        if (body.data != null) {
-                            if(body.data!!.role == IQUser.Role.ADMIN){
-                                val token:String? = body.data!!.token
+                        val user:IQUser = body.data ?: throw Exception("Failed to retrieve user from login Request")
+                        if(body.data!!.role == IQUser.Role.ADMIN){
+                            sharedPreferencesGson.setObjectToSharedPref(body.data!!,getString(R.string.user_info_storage));
+                            Log.d(getString(R.string.app_name),"User Logged In successfully")
 
-                            }
+                        }else{
+                            Log.d(getString(R.string.app_name),"User logged in but its not and admin user")
+                            //TODO Make a popup saying that is not an admin user
                         }
                     }
                     else -> {
@@ -64,7 +72,7 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     fun toastError(e:Exception,context: Context){
         launch(Dispatchers.Main) {
             Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
-            Log.d("Error Occurred:", "${e.message}")
+            Log.d(getString(R.string.error_unexpected_error), "Error Occurred: ${e.message}")
             e.printStackTrace()
         }
     }
